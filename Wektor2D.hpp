8398a496e7efbc77eb8dd76cb806b007634b5b6a
albik123 <127779749+albik123@users.noreply.github.com>
2023-10-25 17:28:48 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,39 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
-};
+private:
+    double x;
+    double y;
+public:
+    Wektor2D(double a = 0, double b = 0)
+    {
+        x = a;
+        y = b;
+        cout << "Wektor: (" << x << ", " << y << ")\n";
+    }
+
+    void setX(double new_x) {
+        x = new_x;
+    }
+
+    double getX() const {
+        return x;
+    }
+
+    void setY(double new_y) {
+        y = new_y;
+    }
+
+    double getY() const {
+        return y;
+    }
+
+    Wektor2D operator+(const Wektor2D& other) const {
+        Wektor2D result(x + other.getX(), y + other.getY());
+        return result;
+    }
+
+    double operator*(const Wektor2D& other) const {
+        return x * other.x + y * other.y;
+    }
+
+};     // Tutaj napisz implementacje klasy Wektor2D
